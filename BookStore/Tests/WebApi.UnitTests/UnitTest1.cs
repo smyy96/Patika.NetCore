2bed586dee6c7368d212105ed317d6d0d391b4d1
smyy <smyy.cskn@hotmail.com>
2022-07-04 19:02:48 +0300

    Test oluşturma

---
+++ BookStore/Tests/WebApi.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace WebApi.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    [Fact]
+    public void Test2()
+    {
+
+    }
+}
